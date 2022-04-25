ec58a5836aaa6e072a221594cbfcace7b3e45dab
dansousamelo <daniel_sousa.unb@outlook.com>
2022-04-25 18:00:54 -0300

    :sparkles: Make Header

--- dashgo/src/pages/index.tsx
+++ dashgo/src/pages/index.tsx
@@ -1,15 +1,38 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react';
+import { Input } from '../components/Form/Input';
 
-export default function Home() {
+export default function SignIn() {
   return (
     <Flex
       w="100vw"
       h="100vh"
       align="center"
-      justify="center">
+      justify="center"
+    >
+      <Flex 
+        as="form"
+        width="100%" 
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDirection="column"
+      >
+        <Stack spacing="4">
+          <Input 
+            type="email"
+            name="email"
+            label="E-mail"
+          />
+          <Input 
+            type="password"
+            name="password"
+            label="Senha"
+          />
+        </Stack>
 
-      <h1>Hello</h1>
-      
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
+      </Flex>
     </Flex>
   )
 }
